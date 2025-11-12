df3d8e5fa5aa7eff7ed23f9fd98213b79bc7647d
Obianuju Ochuba <ochubaju@gmail.com>
2025-11-12 23:21:44 +0000

    add push request schema

--- push-service/app/schemas/NotificationSchema.py
+++ push-service/app/schemas/NotificationSchema.py
@@ -7,12 +7,19 @@ from pydantic import BaseModel
 from app.schemas.UserSchema import UserData
 
 
-class Notification(BaseModel):
-    notification_type: Literal["email", "push"]
-    user_id: uuid
-    template_code: str
-    status: Literal["delivered", "pending", "failed"]
-    variables: UserData
-    request_id: str
-    priority: int
-    metadata: Optional[dict]
+# class Notification(BaseModel):
+#     notification_type: Literal["email", "push"]
+#     user_id: uuid
+#     template_code: str
+#     status: Literal["delivered", "pending", "failed"]
+#     variables: UserData
+#     request_id: str
+#     priority: int
+#     metadata: Optional[dict]
+
+
+class PushRequest(BaseModel):
+    title: str
+    body: str
+
+
