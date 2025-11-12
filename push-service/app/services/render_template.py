f0be5d5c32ca155b62893a3bd5b24ed4f128fc7e
Obianuju Ochuba <ochubaju@gmail.com>
2025-11-12 23:17:40 +0000

    add function to render template

--- push-service/app/services/render_template.py
+++ push-service/app/services/render_template.py
@@ -4,6 +4,7 @@ from jinja2 import Template
 def render_template(template_str: str, context: dict) -> str:
 
     template = Template(template_str)
+
     return template.render(**context)
 
 # template_str = "Hello {{ name }}, your order {{ order_id }} has been shipped!"
